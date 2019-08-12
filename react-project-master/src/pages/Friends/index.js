d6d680ee84ceb67fdd91d4f87fce16bf3726c7d7
Reinachen <735694004@qq.com>
2019-08-12 17:17:28 +0800

    朋友圈页面

---
+++ react-project-master/src/pages/Friends/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: 陈佳
+ * @Date: 2019-08-06 16:45:09
+ * @LastEditTime: 2019-08-12 11:53:09
+ * @LastEditors: 陈佳
+ */
+import React, { Component } from 'react'
+import Events from "../../components/Friends/Events"
+export default class Index extends Component {
+    render() {
+        return (
+            <div >
+              <Events/>
+            </div>
+        )
+    }
+}
